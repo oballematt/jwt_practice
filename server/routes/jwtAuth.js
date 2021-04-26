216c993bf05b225bd95ac560d2997d7fadbe5a6a
Matt <oballematt@gmail.com>
2021-04-26 09:40:20 -0500

    created jwt token.

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const pool = require('../db');
 const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator')
 
 router.post('/register', async (req, res) =>{
     try {
@@ -29,8 +30,12 @@ router.post('/register', async (req, res) =>{
             name, email, bcryptPassword
         ]);
 
-        res.json(newUser.rows[0])
         // //5. generating jwt token
+
+        const token = jwtGenerator(newUser.rows[0].user_id);
+
+        res.json({ token })
+
     } catch (error) {
         console.error('error.message');
         res.status(500).send('Server Error');
--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -1,2 +1,12 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
+
+function jwtGenerator(user_id) {
+    const payload = {
+        user: user_id
+    }
+
+    return jwt.sign(payload, process.env.JWTSECRET, {expiresIn: "1hr"})
+}
+
+module.exports = jwtGenerator
