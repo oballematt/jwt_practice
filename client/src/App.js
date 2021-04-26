a2ffb8c5fc2377b0b084245202c103a70f72c3b3
Matt <oballematt@gmail.com>
2021-04-26 14:20:41 -0500

    added routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
-
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-dom"
 import './App.css';
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import Register from "./components/Register"
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  return (
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route exact path="/login"
+            render={props =>
+              !isAuthenticated ? (
+                <Login {...props} /> ) : (
+                <Redirect to="/dashboard" />
+              )
+            }
+          />
+          <Route exact path="/register"
+            render={props =>
+              !isAuthenticated ? (
+                <Register {...props} /> ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+          <Route exact path="/dashboard"
+            render={props =>
+              !isAuthenticated ? (
+                <Dashboard {...props} /> ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
