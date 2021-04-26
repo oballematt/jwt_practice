23eae7fbe271dd96c34ab551e86016594eb81752
Matt <oballematt@gmail.com>
2021-04-26 10:53:34 -0500

    setting up middleware.

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -2,15 +2,17 @@ const router = require('express').Router();
 const pool = require('../db');
 const bcrypt = require('bcrypt');
 const jwtGenerator = require('../utils/jwtGenerator')
+const validInfo = require('../middleware/validInfo')
+const authorization = require('../middleware/authorization')
 
-router.post('/register', async (req, res) =>{
+router.post('/register', validInfo, async (req, res) => {
     try {
         //1. destructure req.body (name, emai, password)
-        
+
         const { name, email, password } = req.body;
-        
+
         //2. check if user exists (if user exists then throw error)
-       
+
         const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
             email
         ]);
@@ -19,18 +21,19 @@ router.post('/register', async (req, res) =>{
             return res.status(401).send('User already exists');
         };
 
-        
-        // //3. Bcrypt the user PASSWORD
+        //3. Bcrypt the user PASSWORD
         const saltRounds = 10;
         const salt = await bcrypt.genSalt(saltRounds);
 
         const bcryptPassword = await bcrypt.hash(password, salt);
-        // //4. Enter the user inside database
+        
+        //4. Enter the user inside database
+        
         const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [
             name, email, bcryptPassword
         ]);
 
-        // //5. generating jwt token
+        //5. generating jwt token
 
         const token = jwtGenerator(newUser.rows[0].user_id);
 
@@ -42,11 +45,11 @@ router.post('/register', async (req, res) =>{
     };
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', validInfo, async (req, res) => {
     try {
         //1. destructure req.body
 
-        const {email, password} = req.body
+        const { email, password } = req.body
 
         //2. check if user doesnt exist (if not throw error)
 
@@ -54,14 +57,14 @@ router.post('/login', async (req, res) => {
             email
         ]);
 
-        if(user.rows.length === 0){
+        if (user.rows.length === 0) {
             return res.status(401).json("Password or Email is incorrect")
         };
 
         //3. check if incoming password is the same as database password
 
         const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
-        
+
         if (!validPassword) {
             return res.status(401).json("Password or Email is incorrect")
         };
@@ -69,10 +72,20 @@ router.post('/login', async (req, res) => {
         //4. give them jwt token
         const token = jwtGenerator(user.rows[0].user_id);
 
-        res.json({token})
+        res.json({ token })
     } catch (error) {
         console.error(error.message)
+        res.status(500).send("Server Error")
     };
 });
+
+router.get('/is-verify', authorization, async (req, res) => {
+    try {
+        res.json(true)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server Error")
+    }
+})
 
 module.exports = router;
